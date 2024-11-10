16d614506fe3b73198ca34b61be725d1afcbbf49
huan nguyen <74613550+huanboss@users.noreply.github.com>
2024-11-10 17:29:33 +0700

    Add files via upload
    upload vpc module

--- 2.Working with Module and Backend/2.2-backend/1-local-backend/main.tf
+++ 2.Working with Module and Backend/2.2-backend/1-local-backend/main.tf
@@ -1,28 +1,82 @@
-terraform {
-  required_providers {
-    aws ={
-        source = "hashicorp/aws"
-        version = "5.49.0"
-    }
-  }
-    backend "s3" {
-    bucket = "demo-backend-s3-20240607-044346"
-    key = "state1/terraform.tfstate"
-    region = "ap-southeast-1"
-    encrypt = true
-    dynamodb_table = "terraform-lock"
+# Tạo VPC
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
   }
 }
 
-provider "aws" {
-  
+# Tạo Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.vpc_name}-IGW"
+  }
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0ac9397cab55f5044"
-  instance_type = "t3.micro"
+# Tạo public subnets (2 Availability Zones)
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet1_cidr_block
+  availability_zone = var.public_subnet1_az
+  tags = {
+    Name = var.public_subnet1_name
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet2_cidr_block
+  availability_zone = var.public_subnet2_az
+  tags = {
+    Name = var.public_subnet2_name
+  }
+}
+
+# Tạo private subnets (2 Availability Zones)
+
+resource "aws_subnet" "private1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet1_cidr_block
+  availability_zone = var.private_subnet1_az
+  tags = {
+    Name = var.private_subnet1_name
+  }
+}
+
+resource "aws_subnet" "private2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet2_cidr_block
+  availability_zone = var.private_subnet2_az
+  tags = {
+    Name = var.private_subnet2_name
+  }
+}
+
+# Tạo public route table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
   tags = {
-    Name = "HelloWorld"
+    Name = "${var.vpc_name}-Public-Route-Table"
   }
+}
+
+# Liên kết public subnets với public route table
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
 }
